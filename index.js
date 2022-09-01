9b1cb98adbf02863a080cfd5cdf893c13d204f83
shyamksonata <shyamk.sonata@gmail.com>
2022-09-01 23:14:13 +0530

    final

---
+++ index.js
@@ -0,0 +1,43 @@
+const billAmount = document.querySelector("#bill-amount");
+const cashGiven = document.querySelector("#cash-given");
+const checkButton = document.querySelector("#check-button");
+const message = document.querySelector("#error-message");
+const noOfNotes = document.querySelectorAll(".no-of-notes");
+
+const availableNotes = [2000, 500, 100, 20, 10, 5, 1];
+
+checkButton.addEventListener("click", function validateBillAndCashAmount(){
+    hideMessage();
+        if(billAmount.value > 0){
+        if (cashGiven.value >= billAmount.value){
+            const amountToBeReturened = cashGiven.value - billAmount.value;
+            calculateChange(amountToBeReturened);
+
+        }else{
+            showMessage("The Cash Provided should atleast equal to bill anount");
+        }
+
+    }else{
+        showMessage("Invalid Bill Amount")
+
+    }
+
+});
+
+function calculateChange(amountToBeReturened){
+    for (let i=0; i< availableNotes.length; i++){
+        const numberOfNotes = Math.trunc(amountToBeReturened / availableNotes[i] );
+        amountToBeReturened = amountToBeReturened %  availableNotes[i];
+        noOfNotes[i].innerText = numberOfNotes;
+    }
+
+}
+
+function hideMessage() {
+    message.style.display ="none";
+}
+
+function showMessage(msg){
+    message.style.display ="block";
+    message.innerText = msg;
+}
